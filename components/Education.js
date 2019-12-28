dbee074c8bd17118eca88ec0040ae8e6a3a0163f
Geoffrey Liu <main@g-liu.com>
2019-12-27 19:27:24 -0800

    Implement showing actual dates

--- components/Education.js
+++ components/Education.js
@@ -1,3 +1,32 @@
+/* startDateString, endDateString are in YYYY-MM-DD format */
+function displayDate(startDateString, endDateString, collapseCenturies=true) {
+	const startDate = moment(startDateString, 'YYYY-MM-DD');
+
+	if (endDateString == null) {
+		return (
+			<span>since<br /><span className="months">{startDate.format('MMM')}</span> {startDate.format('YYYY')}</span>
+		);
+	}
+
+	const endDate = moment(endDateString, 'YYYY-MM-DD');
+
+	if (startDate.isSame(endDate, 'year')) {
+		return (
+			<span><span className="months">{startDate.format('MMM')}-{endDate.format('MMM')}</span> {startDate.format('YYYY')}</span>
+		);
+	}
+
+	if (collapseCenturies) {
+		return (
+			<span>{startDate.format('YYYY')}-{endDate.format('YY')}</span>
+		)
+	}
+
+	return (
+		<span>{startDate.format('YYYY')} <span className="sm-caps" style={{position: 'relative', top: '-1px'}}>to</span> {endDate.format('YYYY')}</span>
+	);
+}
+
 class Education extends React.Component {
 	constructor(props) {
 		super(props);
@@ -12,9 +41,10 @@ class Education extends React.Component {
 
 	render() {
 		const degreesEarned = this.state.degrees.join('; ');
+		const displayYears = displayDate(this.state.startDate, this.state.endDate, false);
 
 		return [<tr key="eeh">
-					<td className="years" rowSpan="3"><span className="months">Jun-Aug</span> 2015</td>
+					<td className="years" rowSpan="3">{displayYears}</td>
 					<td className="job-title"><h4>{degreesEarned}</h4></td>
 				</tr>,
 				<tr key="eeh2">
--- components/Experience.js
+++ components/Experience.js
@@ -11,12 +11,16 @@ class Experience extends React.Component {
 	}
 
 	render() {
+		const displayYears = displayDate(this.state.startDate, this.state.endDate);
 		return [<tr key="heh">
-					<td className="years" rowSpan="3"><span className="months">Jun-Aug</span> 2015</td>
+					<td className="years" rowSpan="3">{displayYears}</td>
 					<td className="job-title"><h4>{this.state.title}</h4></td>
 				</tr>,
 				<tr key="heh2">
-					<td className="employer" colSpan="2"><a href={this.state.companyUrl}>{this.state.company}</a></td>
+					<td className="employer" colSpan="2">
+						<a href={this.state.companyUrl}>{this.state.company}</a>,&nbsp;
+						<span className="location">{this.state.location}</span>
+					</td>
 				</tr>,
 				<tr key="heh3">
 					<td className="job-description" colSpan="2">
