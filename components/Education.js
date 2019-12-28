028c3d5d2ed783750b674e1316b5f6114b898a60
Geoffrey Liu <main@g-liu.com>
2019-12-27 17:05:52 -0800

    Complete education component, except for date

--- components/Education.js
+++ components/Education.js
@@ -5,22 +5,30 @@ class Education extends React.Component {
 			endDate: props.endDate,
 			degrees: props.degrees,
 			institution: props.institution,
-			loctaionCity: props.loctaionCity,
+			locationCity: props.locationCity,
 			achievements: props.achievements,
 			description: props.description};
 	}
 
 	render() {
+		const degreesEarned = this.state.degrees.join('; ');
+
 		return [<tr key="eeh">
 					<td className="years" rowSpan="3"><span className="months">Jun-Aug</span> 2015</td>
-					<td className="job-title"><h4>{this.state.degrees[0].title}</h4></td>
+					<td className="job-title"><h4>{degreesEarned}</h4></td>
 				</tr>,
 				<tr key="eeh2">
-					<td className="employer" colSpan="2">{this.state.institution}</td>
+					<td className="employer" colSpan="2">{this.state.institution}, {this.state.locationCity}</td>
 				</tr>,
 				<tr key="eeh3">
 					<td className="job-description" colSpan="2">
-						<p>{this.state.achievements[0]}</p>
+						{this.state.achievements && this.state.achievements.length > 0 ?
+							<ul className="bulleted">
+								{this.state.achievements.map((achievement, index) => {
+									return <li key={index}>{achievement}</li>
+								})}
+							</ul>
+						 : null}
 					</td>
 				</tr>];
 	}
